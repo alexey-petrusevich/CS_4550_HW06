21f3ee094fd349ccd77e5f3b66bbf580e3f7cb35
apetrusevich1 <apetrusevich1@gmail.com>
2021-02-27 16:32:14 -0500

    1

--- bulls/lib/bulls/bulls.ex
+++ bulls/lib/bulls/bulls.ex
@@ -254,14 +254,14 @@ defmodule FourDigits.Game do
     IO.inspect(map)
     IO.inspect("playerName")
     IO.inspect(playerName)
-    if (Enum.count(mapKeys) == 0) do
+    if (length(mapKeys) == 0) do
       # if here, playerName somehow not found in the map
       IO.inspect("player name not found????")
       nil
     else
       # check if the value of the first entry in the list of keys in the given map
       # matches given playerName
-      IO.inspect("checking if key = " <> hd(mapKeys) <> " has value of playerName = " <> playerName)
+      IO.inspect("checking if key = ")
       if (Map.get(map, hd(mapKeys)) == playerName) do
         # found the key of the given playerName - return to the caller
         IO.inspect("key found!")
